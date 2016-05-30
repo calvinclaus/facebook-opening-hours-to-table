c6a89b731aa44e11b2c3685e443665d2ba5d6d9e
calvinclaus <calvinclaus@me.com>
2016-05-30 23:00:09 +0200

    added translation tests

--- src/tests/getIntervalsWithHours.test.js
+++ src/tests/getIntervalsWithHours.test.js
@@ -1,13 +1,23 @@
 import expect from "expect";
 import { getIntervalsWithHours } from "../entry.js";
 import { hoursRequester } from "./mocks/mocks.js";
-import intervals from "./mocks/intervals.mock.json";
-import hours from "./mocks/hours.mock.json";
 
-describe("getIntervalWithHours", () => {
-  it("it should return intervals using hours", () => {
+//Mock Data
+import hours from "./mocks/hours.mock.json";
+import intervals from "./mocks/intervals.mock.json";
+import intervalsGer from "./mocks/intervalsGerman.mock.json";
+
+describe("getIntervalsWithHours", () => {
+  it("should return intervals using hours", () => {
     const expected = intervals;
     const actual = getIntervalsWithHours(hours);
+    expect(actual).toEqual(expected);
+  });
+
+  it("should translate correctly", () => {
+    const expected = intervalsGer;
+    const translationDict =  {mon: "Mo", tue: "Di", wed: "Mi", thu: "Do", fri: "Fr", sat: "Sa", sun: "So"}
+    const actual = getIntervalsWithHours(hours, translationDict);
     expect(actual).toEqual(expected);
   });
 });
--- src/tests/getTableWithHoursData.test.js
+++ src/tests/getTableWithHoursData.test.js
@@ -1,7 +1,6 @@
 import expect from 'expect'
 import expectElement from 'expect-element'
 expect.extend(expectElement)
-
 import jsdom from "mocha-jsdom";
 
 import { getTableWithHours } from '../entry.js';
@@ -15,6 +14,13 @@ describe("getTableWithHours", () => {
     const actual = getTableWithHours(hours);
     expect(actual.tagName).toEqual("TABLE");
     expect(actual.innerHTML).toEqual('<tbody><tr><td><span class="days">mon - wed: </span></td><td><span class="numbers">11:00 - 20:30</span></td></tr><tr><td><span class="days">thu - fri: </span></td><td><span class="numbers">11:00 - 21:00</span></td></tr><tr><td><span class="days">sat: </span></td><td><span class="numbers">10:30 - 20:30</span></td></tr><tr><td><span class="days">sun: </span></td><td><span class="numbers">12:00 - 19:00</span></td></tr></tbody>');
+  });
+
+  it('should return a translated table', function () {
+    const translationDict =  {mon: "Mo", tue: "Di", wed: "Mi", thu: "Do", fri: "Fr", sat: "Sa", sun: "So"}
+    const actual = getTableWithHours(hours, translationDict);
+    expect(actual.tagName).toEqual("TABLE");
+    expect(actual.innerHTML).toEqual('<tbody><tr><td><span class="days">Mo - Mi: </span></td><td><span class="numbers">11:00 - 20:30</span></td></tr><tr><td><span class="days">Do - Fr: </span></td><td><span class="numbers">11:00 - 21:00</span></td></tr><tr><td><span class="days">Sa: </span></td><td><span class="numbers">10:30 - 20:30</span></td></tr><tr><td><span class="days">So: </span></td><td><span class="numbers">12:00 - 19:00</span></td></tr></tbody>');
   });
 
 });
